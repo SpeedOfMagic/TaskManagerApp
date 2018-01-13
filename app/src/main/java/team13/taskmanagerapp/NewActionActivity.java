be7b6a2913d976c0354106d4410434342332c067
Kate <tochilina.2002@yandex.ru>
2018-01-14 00:21:00 +0500

    New action Activity and layout added

--- app/src/main/java/team13/taskmanagerapp/NewActionActivity.java
+++ app/src/main/java/team13/taskmanagerapp/NewActionActivity.java
@@ -1,10 +1,16 @@
 package team13.taskmanagerapp;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by kate on 13.01.2018.
  */
 
 public class NewActionActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.new_action);
+    }
 }
