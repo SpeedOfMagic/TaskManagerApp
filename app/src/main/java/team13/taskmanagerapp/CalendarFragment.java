f1e491985dc490f5639c863c8bc6be8f40999196
Anton Lavrov <anton.p.lavrov@gmail.com>
2018-01-19 20:08:55 +0300

    List of tasks

---
+++ app/src/main/java/team13/taskmanagerapp/CalendarFragment.java
@@ -0,0 +1,29 @@
+package team13.taskmanagerapp;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CalendarView;
+
+/**
+ * Created by kate on 16.01.2018.
+ */
+
+public class CalendarFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        getActivity().setTitle("Календарь");
+        View rootView = inflater.inflate(R.layout.calendar, container, false);
+        CalendarView calendar = rootView.findViewById(R.id.calendarView);
+        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // Вызов Fragment с делами на выбранный день
+            }
+        });
+        return rootView;
+    }
+}
